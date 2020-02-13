f0489f2d3be8c144579c3b503d9343dfd1f6ec6b
rizkihariyanto23 <rizkihariyanto23@gmail.com>
2020-02-13 10:18:58 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione voluptates sit velit exercitationem dolor vitae similique ipsa, repudiandae non hic officiis odit minus iure. Consequatur harum maxime quo dignissimos soluta.</p>
 </body>
 </html>
