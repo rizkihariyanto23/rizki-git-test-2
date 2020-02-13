17ad5d4cffbdd0a004362a2af6531fc31e1ed023
rizkihariyanto23 <rizkihariyanto23@gmail.com>
2020-02-13 10:27:27 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione voluptates sit velit exercitationem dolor vitae similique ipsa, repudiandae non hic officiis odit minus iure. Consequatur harum maxime quo dignissimos soluta.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
